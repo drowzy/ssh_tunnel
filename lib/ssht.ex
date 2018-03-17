5aa5b00269d4d1994d221b82e9649f766d5f3e3c
Simon Thörnqvist <simon@plug-n-trade.com>
2018-03-18 00:30:32 +0100

    remove conn module

--- lib/ssht.ex
+++ lib/ssht.ex
@@ -5,8 +5,15 @@ defmodule SSHt do
   @ini_window_size 1024 * 1024
   @max_packet_size 32 * 1024
 
-  defdelegate connect(opts), to: SSHt.Conn
   defdelegate start_link(ref, to), to: SSHt.Tunnel
+
+  def connect(opts \\ []) do
+    host = Keyword.get(opts, :host, "127.0.0.1")
+    port = Keyword.get(opts, :port, 22)
+    ssh_config = defaults(opts)
+
+    :ssh.connect(String.to_charlist(host), port, ssh_config)
+  end
 
   def direct_tcpip(ref, from, to) do
     {orig_host, orig_port} = from
@@ -45,5 +52,17 @@ defmodule SSHt do
       @max_packet_size,
       :infinity
     )
+  end
+
+  defp defaults(opts) do
+    user = Keyword.get(opts, :user, "")
+    password = Keyword.get(opts, :password, "")
+
+    [
+      user_interaction: false,
+      silently_accept_hosts: true,
+      user: String.to_charlist(user),
+      password: String.to_charlist(password)
+    ]
   end
 end
